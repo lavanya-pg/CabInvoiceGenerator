b078f5fcd27bd32b65c758b147bc3c733ae0bab7
lavanya-pg <lavanyapg8@gmail.com>
2021-07-25 22:28:59 +0530

    Initial Commit: Welcome To the CabInvoiceGenerator

---
+++ lib/src/main/java/cabinvoicegenerator/CabInvoiceMain.java
@@ -0,0 +1,9 @@
+package cabinvoicegenerator;
+
+public class CabInvoiceMain 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome to the Cab Invoice Generator Program");
+	}
+}
